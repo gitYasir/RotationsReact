167bfd556d3b63454220c44704cd85eae115e7bc
Yasir <yasir.ibrahim@hotmail.co.uk>
2022-06-16 23:30:11 +0100

    remove button working

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,10 +1,12 @@
 import AddWorkerBtn from "../AddWorkerBtn/AddWorkerBtn";
+import RemoveWorkerBtn from "../RemoveWorkerBtn/RemoveWorkerBtn";
 
 function App() {
   return (
     <div style={{ textAlign: "center" }}>
       <h1>ROTATIONS</h1>
       <AddWorkerBtn />
+      <RemoveWorkerBtn />
     </div>
   );
 }
--- src/data/db.js
+++ src/data/db.js
@@ -1,6 +1,8 @@
-export const workers = [
+const workers = [
   { name: "a", stations: [1, 2, 3, 4] },
   { name: "v", stations: [2, 3, 4] },
   { name: "r", stations: [3, 4] },
   { name: "w", stations: [4, 5, 6, 7, 8] },
 ];
+
+export default workers;
