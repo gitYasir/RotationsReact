d20f9fe95349dd7c14931baa8c901435224b9c45
Yasir <yasir.ibrahim@hotmail.co.uk>
2022-06-17 00:03:30 +0100

    working on saving new workers to db

--- src/Components/AddWorkerBtn/AddWorkerBtn.js
+++ src/Components/AddWorkerBtn/AddWorkerBtn.js
@@ -2,12 +2,30 @@ import { useState } from "react";
 
 function AddWorkerBtn() {
   const [show, setShow] = useState(false);
+  const [workerName, setWorkerName] = useState("");
+  const [workerStations, setWorkerStations] = useState([]);
+
+  function saveWorker() {}
 
   return (
     <>
       {show && (
         <>
-          <input type={"text"} placeholder={"Name"} />
+          <input
+            type={"text"}
+            placeholder={"Name"}
+            onChange={(e) => {
+              setWorkerName(e.target.value);
+            }}
+          />
+          <input
+            type="text"
+            pattern="[0-9 ]+"
+            placeholder="Stations: eg, 20 22 24"
+            onChange={(e) => {
+              setWorkerStations(e.target.value);
+            }}
+          />
           <button>Save</button>
           <button
             onClick={() => {
