de475a4979aabd123639be4f05f2928c27c21558
Yasir <yasir.ibrahim@hotmail.co.uk>
2022-06-18 04:36:04 +0100

    changed show var to shuffle

--- src/Components/ShuffleBtn/ShuffleBtn.js
+++ src/Components/ShuffleBtn/ShuffleBtn.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import workers from "../../data/db";
 
 function ShuffleBtn() {
-  const [show, setShow] = useState(false);
+  const [shuffle, setShuffle] = useState(false);
   const [data, setData] = useState(workers);
 
   function removeWorker(name) {
@@ -33,51 +33,49 @@ function ShuffleBtn() {
     <>
       <button
         onClick={() => {
-          setShow(!show);
+          setShuffle(!shuffle);
         }}
       >
         Shuffle
       </button>
-      {show && (
-        <table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>1st Station</th>
-              <th>2nd Station</th>
-              <th>3rd Station</th>
-              <th>4th Station</th>
-            </tr>
-            {data.map((worker) => {
-              let stationsArr = shuffleArr(worker.stations);
-              return (
-                <tr key={worker.name}>
-                  <td>{worker.name}</td>
-                  {stationsArr[0] ? <td>{stationsArr[0]}</td> : <td></td>}
-                  {stationsArr[1] ? (
-                    <td>{stationsArr[1]}</td>
-                  ) : (
-                    <td>{stationsArr[0]}</td>
-                  )}
-                  {stationsArr[2] ? (
-                    <td>{stationsArr[2]}</td>
-                  ) : (
-                    <td>{stationsArr[0]}</td>
-                  )}
-                  {stationsArr[3] ? (
-                    <td>{stationsArr[3]}</td>
-                  ) : stationsArr[1] ? (
-                    <td>{stationsArr[1]}</td>
-                  ) : (
-                    <td>{stationsArr[0]}</td>
-                  )}
-                  <td onClick={() => removeWorker(worker.name)}>X</td>
-                </tr>
-              );
-            })}
-          </thead>
-        </table>
-      )}
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>1st Station</th>
+            <th>2nd Station</th>
+            <th>3rd Station</th>
+            <th>4th Station</th>
+          </tr>
+          {data.map((worker) => {
+            let stationsArr = shuffleArr(worker.stations);
+            return (
+              <tr key={worker.name}>
+                <td>{worker.name}</td>
+                {stationsArr[0] ? <td>{stationsArr[0]}</td> : <td></td>}
+                {stationsArr[1] ? (
+                  <td>{stationsArr[1]}</td>
+                ) : (
+                  <td>{stationsArr[0]}</td>
+                )}
+                {stationsArr[2] ? (
+                  <td>{stationsArr[2]}</td>
+                ) : (
+                  <td>{stationsArr[0]}</td>
+                )}
+                {stationsArr[3] ? (
+                  <td>{stationsArr[3]}</td>
+                ) : stationsArr[1] ? (
+                  <td>{stationsArr[1]}</td>
+                ) : (
+                  <td>{stationsArr[0]}</td>
+                )}
+                <td onClick={() => removeWorker(worker.name)}>X</td>
+              </tr>
+            );
+          })}
+        </thead>
+      </table>
     </>
   );
 }
