b9b81a04490404e78cd18f18426f6480ecb46a93
Yasir <yasir.ibrahim@hotmail.co.uk>
2022-07-05 04:27:10 +0100

    changed background color

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,9 +1,10 @@
 import AddWorkerBtn from "../AddWorkerBtn/AddWorkerBtn";
 import ShuffleBtn from "../ShuffleBtn/ShuffleBtn";
+import css from "./app.module.css";
 
 function App() {
   return (
-    <div style={{ textAlign: "center" }}>
+    <div className={css.main}>
       <h1>ROTATIONS</h1>
       <AddWorkerBtn />
       <ShuffleBtn />
